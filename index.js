3f388e876ca2ad1e58a2d51fdfa1178e86ad3a17
Akshay <akshaydhola3527@gmail.com>
2024-10-19 13:04:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const { greet } = require('rust_module');
 const app = express();
 const PORT = 3000;
 
+app.get('/', (req, res) => {
+    res.send("go to /greet/your_name");
+});
+
 app.get('/greet/:name', (req, res) => {
     const name = req.params.name;
     const message = greet(name);
